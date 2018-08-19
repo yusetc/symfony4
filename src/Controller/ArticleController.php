5420a084ef65547f3e6a8b0e0cab165a377dad7a
Yuset Calzadilla <yuset.calzadilla@gmail.com>
2018-08-19 17:11:24 -0500

    My first commit for symfony 4

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArticleController extends AbstractController{
+    /**
+     * @Route("/")
+    */
+    public function index() {
+        //return new Response ('<html><body>Hello</body></html>');
+
+        return $this->render('articles/index.html.twig');
+    }
+}
